cfa2d7488f41eeced74fb33dd6413f14e1e92cff
LinaChen <sakura0477@gmail.com>
2025-03-15 15:59:02 +0800

    feat: add react hook form, validate login form

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,20 +1,64 @@
+import { useForm } from "react-hook-form";
+
 const Home = () => {
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log('login');
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        mode: 'onTouched'
+    })
+    const onSubmit = (data) => {
+        console.log('login', data);
     };
     return (<>
         <div className="row justify-content-center">
             <div className="col-6">
                 <h2 className="h4 text-center">請先登入</h2>
-                <form onSubmit={(e) => onSubmit(e)}>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="email" placeholder="輸入信箱" />
-                        <label for="email">信箱</label>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-floating mb-3">
+                        <input
+                            type="email"
+                            className={`form-control ${errors.username && 'is-invalid'}`}
+                            id="email"
+                            placeholder="請輸入信箱"
+                            {...register('username', {
+                                required: {
+                                    value: true,
+                                    message: '信箱為必填'
+                                },
+                                pattern: {
+                                    value: /^\S+@\S+$/i,
+                                    message: '信箱格式不正確'
+                                }
+                            })}
+                        />
+                        <label htmlFor="email">信箱</label>
+                        {
+                            errors.username && <div className="invalid-feedback">{errors.username.message}</div>
+                        }
                     </div>
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="password" placeholder="請輸入密碼" />
-                        <label for="password">密碼</label>
+                    <div className="form-floating mb-3">
+                        <input
+                            type="password"
+                            className={`form-control ${errors.password && 'is-invalid'}`}
+                            id="password"
+                            placeholder="請輸入密碼"
+                            {...register('password', {
+                                required: {
+                                    value: true,
+                                    message: '密碼為必填'
+                                },
+                                minLength: {
+                                    value: 6,
+                                    message: '長度不小於 6 碼'
+                                }
+                            })}
+                        />
+                        <label htmlFor="password">密碼</label>
+                        {
+                            errors.password && <div className="invalid-feedback">{errors.password.message}</div>
+                        }
                     </div>
                     <button type="submit" className="btn btn-primary py-2 w-100">登入</button>
                 </form>
