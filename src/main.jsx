225c374421f09806138eb6fe2fefb0abc57cadf4
LinaChen <LinaChen@uitc.com.tw>
2025-03-19 15:13:40 +0800

    fix: route path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { createHashRouter, RouterProvider } from 'react-router';
 import 'bootstrap';
 import './assets/scss/all.scss';
-import routes from '../routes';
+import routes from './routes';
 const router = createHashRouter(routes);
 
 createRoot(document.getElementById('root')).render(
